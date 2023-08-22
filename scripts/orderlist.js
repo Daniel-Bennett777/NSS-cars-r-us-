ef811d3c0f3159be04ac775ec40fb8a62b4ec60b
Daniel Bennett <danielpsalmbennett@yahoo.com>
2023-08-22 10:43:15 -0500

    yes

--- scripts/orderlist.js
+++ scripts/orderlist.js
@@ -4,10 +4,18 @@ export const Orders = async () => {
     
     let ordersHTML = orders.map(
         (order) => {
-            const orderPrice = order.paintColor.price + order.interiorOption.price + order.wheelOption.price + order.techPackage.price
+            let orderPrice = order.paintColor.price + order.interiorOption.price + order.wheelOption.price + order.techPackage.price
+            let carType = order.carType || "car"; // Default to "ring" if not specified
+
+            // Update price based on jewelry type
+            if (carType === "suv") {
+                orderPrice *= 1.5;
+            } else if (carType === "truck") {
+                orderPrice *= 2.25;
+            }
 
             return `<div>
-            order #${order.id} cost ${orderPrice}
+            order #${order.id} (${carType})cost ${orderPrice.toFixed(2)}
         </div>`
         }
     )
--- scripts/transient.js
+++ scripts/transient.js
@@ -1,40 +1,44 @@
-const transientState = {
-    "paintColorId" : 0,
-    "interiorOptionId" : 0,
-    "techPackageId": 0,
-    "wheelOptionId" :0,
-     
+const transientState = new Map()
+    transientState.set("paintColorId", 0),
+    transientState.set ("interiorOptionId", 0),
+    transientState.set("techPackageId", 0),
+    transientState.set("wheelOptionId", 0),
+    transientState.set("carType", "car")
     
+     
 
-}
+export const setCarTypeChoice = (chosenType) => {
+    transientState.set("carType", chosenType);
+    console.log(transientState);
+};
 export const setPaintChoice = (chosenOwnership) => {
-    transientState.paintColorId = chosenOwnership 
+    transientState.set("paintColorId", chosenOwnership);
     console.log(transientState)
 }
 
 export const setInteriorChoice = (taken) => {
-    transientState.interiorOptionId = taken
+    transientState.set("interiorOptionId", taken);
     console.log(transientState)
 }
 
 export const setTechChoice = (yes) => {
-    transientState.techPackageId = yes
+    transientState.set("techPackageId", yes);
     console.log(transientState)
 }
 export const setWheelChoice = (no) => {
-    transientState.wheelOptionId = no
+    transientState.set("wheelOptionId", no)
     console.log(transientState)
 }
 
 export const placeOrder = async () => {
-
+const transientObj = Object.fromEntries(transientState)
 
     const postOptions = {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
         },
-        body: JSON.stringify(transientState)
+        body: JSON.stringify(transientObj)
     }
 
     const response = await fetch ("http://localhost:8088/orders", postOptions)
